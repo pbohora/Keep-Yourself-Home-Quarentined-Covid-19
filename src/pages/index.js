2a902892f874813194e7830ad0c54d44cfbad1d4
pbohora <pradeepbohora26@gmail.com>
2020-04-07 00:12:23 +0300

    create geoJSON data

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,31 @@ const IndexPage = () => {
     }
 
     const { data = [] } = response
-    console.log(data)
+
+    const hasData = Array.isArray(data) && data.length > 0
+
+    if (!hasData) {
+      return
+    }
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country
+        const { lat, long: lng } = countryInfo
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat],
+          },
+        }
+      }),
+    }
+    console.log(geoJson)
   }
 
   const mapSettings = {
