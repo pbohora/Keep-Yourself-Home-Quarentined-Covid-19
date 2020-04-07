16a0906cecc104dcb942d7028e88336da8700309
pbohora <pradeepbohora26@gmail.com>
2020-04-07 19:28:41 +0300

    Add the data to the map, style the marker and add dark mode style of map from mopbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Container from 'components/Container'
 import Map from 'components/Map'
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 }
 const CENTER = [LOCATION.lat, LOCATION.lng]
 const DEFAULT_ZOOM = 2
@@ -57,11 +57,56 @@ const IndexPage = () => {
       }),
     }
     console.log(geoJson)
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature
+        let updatedFormatted
+        let casesString
+
+        const { country, updated, cases, deaths, recovered } = properties
+
+        casesString = `${cases}`
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString()
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html,
+          }),
+          riseOnHover: true,
+        })
+      },
+    })
+
+    geoJsonLayers.addTo(map)
   }
 
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
+    defaultBaseMap: 'Mapbox',
     zoom: DEFAULT_ZOOM,
     mapEffect,
   }
@@ -73,20 +118,6 @@ const IndexPage = () => {
       </Helmet>
 
       <Map {...mapSettings}></Map>
-
-      <Container type="content" className="text-center home-start">
-        <h2>Still Getting Started?</h2>
-        <p>Run the following in your terminal!</p>
-        <pre>
-          <code>
-            gatsby new [directory]
-            https://github.com/colbyfayock/gatsby-starter-leaflet
-          </code>
-        </pre>
-        <p className="note">
-          Note: Gatsby CLI required globally for the above command
-        </p>
-      </Container>
     </Layout>
   )
 }
